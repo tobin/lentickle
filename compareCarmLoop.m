f05b15ac69f0938cb3f8998b9b78360927e8783f
Tobin Fricke <tobin@pas.rochester.edu>
2011-06-29 16:41:41 -0500

    Compare DARM and CARM loops to measurements

--- compareCarmLoop.m
+++ compareCarmLoop.m
@@ -3,7 +3,7 @@
 %% Run Lentickle
 setupLentickle;
 
-f = logspace(log10(20),log10(8000), 201).';
+f = logspace(log10(20),log10(100e3), 201).';
 
 inPower = 10;
 darmoffset = 10e-12;
@@ -12,21 +12,21 @@ rslt = getEligoResults(f,inPower,darmoffset);
 
 %% Read a measurement
 
-data = dlmread('eligomeasurements/OMCbackscatter/H1/07-07/darmolg.txt');
+fn = 'eligomeasurements/DCnoisecouplings/freq/L1/2010-06-02/CM000.ASC';
+data = textread(fn, '', 'headerlines', 17);
 
-msmt = struct('f', [], 'H', []);
 msmt.f = data(:,1);
-msmt.H = data(:,2) + 1i*data(:,3);
+msmt.H = (10.^(data(:,2)/20)) .* exp(1i*data(:,3)*pi/180);
 
 %% Plot them together
 
-clg = pickleTF(rslt, 'DARM', 'DARM', 'cl');
+clg = pickleTF(rslt, 'CM', 'CM', 'cl');
 olg = 1 - 1./clg;
 
 subplot(2,1,1);
 semilogx(f, db(olg), '-', msmt.f, db(msmt.H), '.-');  
 legend('model', 'measurement');
-title('H1 DARM OLG');
+title('L1 CARM OLG');
 ylabel('dB');
 subplot(2,1,2);
 semilogx(f, angle(olg)*180/pi, '-', msmt.f, angle(msmt.H)*180/pi, '.-');
@@ -34,4 +34,4 @@ set(gca,'YTick', 45*(-4:4));
 ylabel('degrees');
 xlabel('Hz');
 
-print -dpdf compareDarmLoopH1.pdf
+print -dpdf compareCarmLoopL1.pdf
