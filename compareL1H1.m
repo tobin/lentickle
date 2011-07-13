4714f84b858c686b83dd65ecebe8ed5cb7f8c70a
Tobin Fricke <tobin@pas.rochester.edu>
2011-07-12 19:31:16 -0500

    compare L1 and H1 noise couplings

---
+++ compareL1H1.m
@@ -0,0 +1,52 @@
+function compareL1H1(msmt_root)
+% Compare L1 and H1 noise coupling measurements
+% 
+% compareL1H1('/home/tobin/projects/eligomeasurements/');
+
+if nargin<1
+    msmt_root = '/home/tobin/projects/eligomeasurements/';
+end
+
+msmts.L1 = load_msmts(msmt_root, 'L1');
+msmts.H1 = load_msmts(msmt_root, 'H1');
+
+msmt_names = {'laserAM', 'laserFM', 'oscAM', 'oscPM'};
+
+for ii=1:length(msmt_names);
+    name = msmt_names{ii};
+    subplot(2,2,ii);
+    compare_couplings(msmts, name);    
+    %orient landscape
+    %print('-dpdf', ['compareL1H1-' name '.pdf']);
+end
+orient landscape
+print('-dpdf', 'figures/compareL1H1.pdf');
+end
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+function compare_couplings(msmts, name)
+
+plot_msmt(msmts.L1.(name), 2);
+plot_msmt(msmts.H1.(name), 5);
+hold off
+caxis([-20 20]);
+%colorbar
+xlabel('frequency [Hz]');
+ylabel(msmts.L1.(name)(1).units);
+grid on
+names = struct('laserAM', 'laser amplitude noise coupling', ...
+               'laserFM', 'laser frequency noise coupling', ...
+               'oscAM',   'oscillator amplitude noise coupling', ...
+               'oscPM',   'oscillator phase noise coupling');
+title(names.(name));           
+end
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+function plot_msmt(msmt, linewidth)
+choosecolor = @(offset) interp1(linspace(-20, 20, 64), jet, offset);
+for ii=1:length(msmt)
+    loglog(msmt(ii).f, abs(msmt(ii).H), ...
+        'LineWidth', linewidth, 'color', choosecolor(msmt(ii).x0));
+    hold all
+end
+end
